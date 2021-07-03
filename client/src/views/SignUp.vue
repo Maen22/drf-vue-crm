c8b7c0a4186888ce9cb271a8029d6dd64c79f6be
Maen22 <m3n.ibregeth@gmail.com>
2021-07-03 14:41:23 +0300

    added error validation for the sign up page.

--- client/src/views/SignUp.vue
+++ client/src/views/SignUp.vue
@@ -4,12 +4,17 @@
       <div class="column is-4 is-offset-4">
         <h1 class="title">Sign up</h1>
         <!-- Sign Up Form -->
-        <form>
+        <form @submit.prevent="formSubmit">
           <!-- Email Input -->
           <div class="field">
             <label>Email</label>
             <div class="control">
-              <input type="email" name="email" class="input" />
+              <input
+                type="email"
+                name="email"
+                class="input"
+                v-model="username"
+              />
             </div>
           </div>
 
@@ -17,15 +22,30 @@
           <div class="field">
             <label>Password</label>
             <div class="control">
-              <input type="password" name="password1" class="input" />
+              <input
+                type="password"
+                name="password1"
+                class="input"
+                v-model="password1"
+              />
             </div>
           </div>
           <!-- Confirm Password Input -->
           <div class="field">
             <label>Repeat Password</label>
             <div class="control">
-              <input type="password" name="password2" class="input" />
+              <input
+                type="password"
+                name="password2"
+                class="input"
+                v-model="password2"
+              />
             </div>
+          </div>
+
+          <!-- Errors -->
+          <div class="notification is-danger" v-if="errors.length">
+            <p v-for="error in errors" v-bind:key="error">- {{ error }}</p>
           </div>
 
           <!-- Submit Button -->
@@ -40,9 +60,36 @@
   </div>
 </template>
 
-
 <script>
+// import axios from 'axios'
+// import { toast } from 'bulma-toast'
+
 export default {
-  name: "SignUp",
-};
+  name: 'SignUp',
+  data() {
+    return {
+      username: '',
+      password1: '',
+      password2: '',
+      errors: [],
+    }
+  },
+  methods: {
+    formSubmit() {
+      this.errors = []
+
+      if (this.username === '') {
+        this.errors.push('the username is missing.')
+      }
+
+      if (this.password1 === '') {
+        this.errors.push('the password is too short.')
+      }
+
+      if (this.password1 !== this.password2) {
+        this.errors.push('the passwords are not mathching.')
+      }
+    },
+  },
+}
 </script>
